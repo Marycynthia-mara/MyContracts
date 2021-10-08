30bc11c0abd208864ceab46a2e0731302c060684
Amarachi <49257104+Marycynthia-mara@users.noreply.github.com>
2021-10-08 05:13:12 +0100

    Created contract2ContractCall using import
    ContractA interacts with the function of ContractB which was imported from ContractB-imported.sol

--- ContractB-imported.sol
+++ ContractB-imported.sol
@@ -1,4 +1,6 @@
 // SPDX-License-Identifier: MIT
+
+// this contract is imported by other contracts
 pragma solidity ^0.8.0;
 
 contract ContractB {
--- contract2ContractCall-import.sol
+++ contract2ContractCall-import.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "ContractB-imported.sol";
 
 // contracts in diffrent files interact
-// here ContractA access the function of ContractB by referenceing the inside of the function of ContractB returnSomething()
+// here ContractA access the function of ContractB by referenceing the inside of the function of ContractB "returnSomething()"
 contract ContractA {
     address contractBAddress;
     
